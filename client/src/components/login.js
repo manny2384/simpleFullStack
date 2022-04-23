a2ec6f3758cc61aac7f3b5ff250ff7dd190bcbf3
manny2384 <gonzalezeh2015@gmail.com>
2022-04-23 15:06:57 -0700

    basic login n signup api call

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,19 +1,40 @@
-function handleSubmit(){
-    fetch("", ()=>{
-
-    });
-}
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 function Login(){
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleClick = () => {
+
+        console.log(`username / password = ${username} / ${password}`)
+        if(username.trim().length === 0 || password.trim().length === 0){
+            console.log("username or password emtpy");
+            alert("username or password empty, please fill");
+            return;
+        }
+
+        axios.post("http://localhost:4000/login?", {
+            username: username,
+            password: password
+        
+        }).then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.log(error);
+        })
+
+    }
+
     return(<div className="login">
         
         <div className="login-form">
-            <form>
-                <label>username: <input type="text" /> </label>
-                <label>password: <input type="password" /> </label>
-                <input type="submit" value="Submit" />
-            </form>
+            
+                <label>username: <input type="text" onChange={e => setUsername(e.target.value)} /> </label>
+                <label>password: <input type="password" onChange={e => setPassword(e.target.value)} /> </label>
+                <button onClick={handleClick}> Submit </button>
+           
         </div>
 
     </div>)
--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,26 +1,67 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+function handleSubmit(props){
+    console.log("Attempting user signup following : ", props);
+
+    axios.post("http://localhost:4000/signup?", {
+        username: props.username,
+        email: props.email,
+        password: props.password
+    
+    }).then((response) => {
+        console.log(response);
+    }).catch((error) => {
+        console.log(error);
+    })
+
+
+
+
+}
 
 function Signup(){
 
-    const [user, setUser] = useState([]);
+    const [user, setUser] = useState({});
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const signUser = (e) => {
-        console.log("signing user", e);
-        // fetch
-            // then res to json
-            // then do somehting with res
+    const handleClick = () =>{
+        console.log("making api call...");
+        
+        // check if strings are only spaces
+        if(username.trim().length === 0 || email.trim().length === 0 || password.trim().length === 0){
+            console.log("Some fields are empty, please try again");
+            alert("Fill appropriate fields");
+            return;
+        }
+
+
+
+        axios.post("http://localhost:4000/signup?", {
+            username: username,
+            email: email,
+            password: password
+        
+        }).then((response) => {
+            console.log(response);
+        }).catch((error) => {
+            console.log(error);
+        })
     }
 
-
     return(<div>
-        <div className="signup- form">
-            <form onSubmit={signUser("h")}>
-                <label>username: <input type="text" /> </label>
-                <label>email: <input type="email" /></label>
-                <label>password: <input type="password" /> </label>
-                <input type="submit" value="Submit" />
-            </form>
+
+        <div className="signup-form">
+   
+            <label>username: <input id="signup-form-username" type="text" onChange={e=>setUsername(e.target.value)} /> </label>
+            <label>email: <input id="signup-form-email" type="email" onChange={e=>setEmail(e.target.value)} /></label>
+            <label>password: <input id="signup-form-password" type="password" onChange={e=>setPassword(e.target.value)} /> </label>
+            <button value="Submit" onClick={handleClick} > Submit </button>
+          
         </div>
+
     </div>)
 }
 
