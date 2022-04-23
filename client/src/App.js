3b00f38ec8bdd5073c60c47dd9291a83488aedb2
manny2384 <gonzalezeh2015@gmail.com>
2022-04-23 16:42:42 -0700

    init ui design

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,43 @@ import Home from "./components/home";
 function App() {
 
   const [state, setState] = useState("home");
+  const [nav, setNav] = useState(true);
+  
+  const handleNavBar = () => {
+    console.log("re-set nav ");
+    
+    if(nav){
+      setNav(!nav);
+      document.getElementsByClassName('item2')[0].style.display = "none";
+      document.getElementsByClassName('navBarContent')[0].style.display = "flex";
+    }
+    
+    else{
+      setNav(!nav);
+      document.getElementsByClassName('item2')[0].style.display = "initial";
+      document.getElementsByClassName('navBarContent')[0].style.display = "none";
+    }
 
+
+
+  }
 
   return (
     <div className="App">
       <section className='navBar' >
-        <button onClick={()=> setState("home")}> Home </button>
-        <button onClick={()=> setState("login")}> Login </button>
-        <button onClick={()=> setState("signup")}> Signup </button>
+        
+        <div className='navBar-icon' onClick={handleNavBar}> 
+          <span className='item1'></span>
+          <span className='item2'></span>
+          <span className='item3'></span>
+
+        </div>
+
+        <div className="navBarContent">
+          <button onClick={()=> setState("home")}> Home </button>
+          <button onClick={()=> setState("login")}> Login </button>
+          <button onClick={()=> setState("signup")}> Signup </button>
+        </div>      
       </section>
 
       
