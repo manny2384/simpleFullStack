0bf24a82561c754deb40baf948eacf34e8e20568
manny2384 <gonzalezeh2015@gmail.com>
2022-05-04 12:57:06 -0700

    netlify code clean up

--- client/src/components/users.js
+++ client/src/components/users.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 function Users(props){
     const [list, setList] = useState([{'username':'1'},{'username':'2'},{'username':'3'},{'username':'1'},{'username':'2'},{'username':'3'}]);
     const [users, setUsers] = useState();
-/*
+
     useEffect(() => {
         axios.get("http://localhost:4000/users", {})
         .then((response) => {
@@ -15,7 +15,7 @@ function Users(props){
         })
 
     }, []);
-*/
+
     var Users;
     useEffect(()=>{
         Users = list.map((x)=>{
